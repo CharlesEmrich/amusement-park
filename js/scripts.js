2bb912612d5a5dd7b12084a09f2ff3095083ca3c
Charles Emrich and Michelle Poterek <charles+michelle@>
2017-04-27 14:31:03 -0700

    fix hide/show code and qualifications

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,29 @@ $(function() {
     if ((userFeet === 4 && userInches >= 8) || (userFeet > 4)) {
       // show Majestic
       console.log("Majestic = Yes.");
+      $("#majestic").slideDown();
+    } else {
+      $("#majestic").slideUp();
     }
-    if ((userFeet === 5 && userInches >= 2) || (userFeet > 6)) {
+
+    if ((userFeet === 5 && userInches >= 2) || (userFeet > 5)) {
       // show Tolerable
       console.log("Tolerable = Yes.");
+      $("#tolerable").slideDown();
+    } else {
+      $("#tolerable").slideUp();
     }
-    if ((userFeet === 5 && userInches >= 5) || (userFeet === 6 && userInches <= 5)) {
+
+    if (
+      ((userFeet === 5 && userInches >= 5) || (userFeet > 5))
+       &&
+       ((userFeet === 6 && userInches <= 5) || (userFeet < 6))
+       ) {
       // show Terrifying
       console.log("Terrifying = Yes.");
+      $("#terrifying").slideDown();
+    } else {
+      $("#terrifying").slideUp();
     }
 
   });
